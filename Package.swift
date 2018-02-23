b719088ce871bffa09e540a7c81437e6c9063f7d
fidetro <zykzzzz@hotmail.com>
2018-02-23 12:44:41 +0800

    add perfect-logger

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
 import PackageDescription
 
+
 let package = Package(
-	name: "PerfectFFDB",
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3)
-	],
-	exclude:["Podfile","Podfile.lock","Pods","src","Swift-FFDB","Swift-FFDB.xcodeproj","Swift-FFDB.xcworkspace","Swift-FFDBTests","SwiftFFDB.podspec"]
+    name: "PerfectFFDB",
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
+           .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git",majorVersion: 3)
+    ],
+    exclude:[]
 )
--- Sources/PerfectFFDB/FFDB.swift
+++ Sources/PerfectFFDB/FFDB.swift
@@ -5,14 +5,14 @@
 //  Created by Fidetro on 2017/8/20.
 //  Copyright © 2017年 Fidetro. All rights reserved.
 //
+
+import PerfectLogger
 func printDebugLog<T>(_ message: T,
                       file: String = #file,
                       method: String = #function,
                       line: Int = #line)
 {
-    #if DEBUG
-        print("\(file)[\(line)], \(method): \(message)")
-    #endif
+    LogFile.error("\(file)[\(line)], \(method): \(message)", logFile: "FFDBError.txt")
 }
 public struct FFDB {
     static var connect = PerfectMySQLConnect.self
